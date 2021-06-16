b906aae8f24878f817eb597a3e7aa9a0e8cdde5c
Gadzeman <ostap778800@gmail.com>
2021-06-16 15:31:33 +0300

    Attempt to decorate modal window

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,23 +1,28 @@
 import React, {useState} from 'react';
 import "./Menu.css"
 import "./MenuMedia.css"
+import ModalWindow from "../ModalWindow/ModalWindow";
 
 const Menu = () => {
     const [burgerActive, setBurgerActive] = useState(false)
+    const [modalActive, setModalActive] = useState(false)
     return (
-        <div className={"menu-wrap"}>
-            <div className={burgerActive ? "menu-burger burger-active" : "menu-burger"} onClick={() => setBurgerActive(!burgerActive)}>
-                <span>{/*burger menu for media*/}</span>
+        <>
+            <div className={"menu-wrap"}>
+                <div className={burgerActive ? "menu-burger burger-active" : "menu-burger"} onClick={() => setBurgerActive(!burgerActive)}>
+                    <span>{/*burger menu for media*/}</span>
+                </div>
+                <div className={burgerActive ? "menu-nav burger-active" : "menu-nav"}>
+                    <h3>Home</h3>
+                    <h3>About</h3>
+                    <h3>Contacts</h3>
+                </div>
+                <div className={"menu-logo"} onClick={() => setModalActive(true)}>
+                    <h1>Modal Window</h1>
+                </div>
             </div>
-            <div className={burgerActive ? "menu-nav burger-active" : "menu-nav"}>
-                <h3>Home</h3>
-                <h3>About</h3>
-                <h3>Contacts</h3>
-            </div>
-            <div className={"menu-logo"}>
-                <h1>Modal Window</h1>
-            </div>
-        </div>
+            <ModalWindow active={modalActive} setActive={setModalActive} />
+        </>
     );
 };
 
